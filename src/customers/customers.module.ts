0c4c6133ab9a199b265f21543f501f79b2aab9b3
Potato <poltoradnevone@gmail.com>
2022-09-25 18:31:22 +0400

    applying middleware

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,35 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { ValidateCustomerMiddleware } from './middlewares/validate-customer.middleware';
 import { CustomersService } from './services/customers/customers.service';
 
 @Module({
   controllers: [CustomersController],
-  providers: [CustomersService]
+  providers: [CustomersService],
 })
-export class CustomersModule {}
+export class CustomersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateCustomerMiddleware)
+      .exclude({
+        path: 'api/customers/:id',
+        method: RequestMethod.GET,
+      })
+      .forRoutes(
+        CustomersController,
+        // {
+        //   path: '/customers/:id',
+        //   method: RequestMethod.GET,
+        // },
+        // {
+        //   path: '/customers',
+        //   method: RequestMethod.GET,
+        // },
+      );
+  }
+}
